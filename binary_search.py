eba44fec46f23662bd22ec2fcde1399f8a569de8
daniildn <dosindaniilm@gmail.com>
2022-01-08 22:57:05 +0300

    binary_searching_sec

--- binary_search.py
+++ binary_search.py
@@ -14,5 +14,18 @@ def binary_search(ls: iter, item):
         else:
             return None
 
+def binary_searching(ls, item):
+    low = 0
+    high = len(ls)-1
+    while low <= high:
+        mid_ind = (low + high)//2
+        guess = ls[mid_ind]
+        if guess == item:
+            return mid_ind
+        elif guess<item:
+            low = mid_ind + 1
+        elif guess>item:
+            high = mid_ind - 1
 
-print(binary_search([1, 2, 3, 4, 5, 6, 7, 8], 8))
+
+print(binary_searching([1, 2, 3, 4, 5, 6, 7, 8, 9], 9))
