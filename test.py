e362a2f54b3fe75f780e0d8f63adeafb8ba06256
daniildn <dosindaniilm@gmail.com>
2022-01-08 19:03:01 +0300

    another war

--- test.py
+++ test.py
@@ -246,3 +246,24 @@ def factorial(x):
 
 
 print(factorial(5))
+
+
+def comp(array1, array2):
+    try:
+        return sorted([i ** 2 for i in array1]) == sorted(array2)
+    except:
+        return False
+
+
+a1 = [121, 144, 19, 161, 19, 144, 19, 11]
+a2 = [11 * 11, 121 * 121, 144 * 144, 19 * 19, 161 * 161, 19 * 19, 144 * 144, 19 * 19]
+print(comp(a1, a2), True)
+a1 = [121, 144, 19, 161, 19, 144, 19, 11]
+a2 = [11 * 21, 121 * 121, 144 * 144, 19 * 19, 161 * 161, 19 * 19, 144 * 144, 19 * 19]
+print(comp(a1, 2), False)
+a1 = [121, 144, 19, 161, 19, 144, 19, 11]
+a2 = [11 * 11, 121 * 121, 144 * 144, 190 * 190, 161 * 161, 19 * 19, 144 * 144, 19 * 19]
+print(comp(a1, a2), False)
+a1 = [2, 2, 3]
+a2 = [4, 9, 9]
+print(comp(a1, a2), False)
